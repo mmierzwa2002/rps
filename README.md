1bb75e14511a4efcd662b809a8c33e52d4d7f631
Michal <123924536+mmierzwa2002@users.noreply.github.com>
2023-08-17 02:03:01 +0100

    Add task solution

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rps
+Classic Rock Paper Scissors game, it is played in the console.
+This project is part of The Odin Project course.
--- script.js
+++ script.js
@@ -1,7 +1,46 @@
-function getComputerChoice() {
+let playerScore = 0;
+let pcScore = 0;
+
+function getComputerChoice() { //generate pc choice by selecting random array item. array contains the available game choices
     const choices = ["Rock", "Paper", "Scissors"];
     let arrPos = Math.floor(Math.random() * 3);
     return choices[arrPos];
 }
 
-console.log(getComputerChoice());
+function game() {
+    for (let i = 1; i <= 5; i++) { //play 5 rounds
+        let computerSelection = getComputerChoice(); //generate inputs
+        let playerSelection = prompt();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    if (pcScore > playerScore) {
+        console.log(`The 5 rounds are over. Unfortunately you lost. The final score was ${pcScore}-${playerScore}.`);
+    } else if (playerScore > pcScore) {
+        console.log(`The 5 rounds are over. Well done! You won. The final score was ${playerScore}-${pcScore}.`);
+    } else {
+        console.log(`The 5 rounds are over. You were tied ${pcScore}-${playerScore}`);
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    let lowerPlayerSelection = playerSelection.toLowerCase(); //make case insensitivity for user
+    let lowerComputerSelection = computerSelection.toLowerCase(); //make user choice with first letter capitalized. looks better when announcing winner
+    let capPlayerSelection = lowerPlayerSelection.charAt(0).toUpperCase() + lowerPlayerSelection.slice(1);
+    if ((lowerPlayerSelection === "rock" && computerSelection === "Scissors") || //win cases
+        (lowerPlayerSelection === "paper" && computerSelection === "Rock")    ||
+        (lowerPlayerSelection === "scissors" && computerSelection === "Paper" )) {
+            playerScore += 1;
+            return `You win! ${capPlayerSelection} beats ${computerSelection}. Congratulations!`;
+        } else if ((lowerPlayerSelection === "rock" && computerSelection === "Paper") || //lose cases
+                   (lowerPlayerSelection === "paper" && computerSelection === "Scissors") ||
+                   (lowerPlayerSelection === "scissors" && computerSelection === "Rock")) {
+                    pcScore += 1;
+                        return `You lost! ${computerSelection} beats ${capPlayerSelection}. Better luck next time!`;
+                   } else if (lowerPlayerSelection === lowerComputerSelection) { // tie case
+                        return `It's a tie! Both players chose ${computerSelection}.`;
+                   } else {
+                        return `There has been an error. Your input is wrong, try again.`
+                   }
+}
+
+game();
