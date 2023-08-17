6ee6334747f5bc3b69cf395fbe8f3e79d42c1eb0
Michal <123924536+mmierzwa2002@users.noreply.github.com>
2023-08-17 02:10:35 +0100

    Cleanup lines

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function game() {
         let playerSelection = prompt();
         console.log(playRound(playerSelection, computerSelection));
     }
+    
     if (pcScore > playerScore) {
         console.log(`The 5 rounds are over. Unfortunately you lost. The final score was ${pcScore}-${playerScore}.`);
     } else if (playerScore > pcScore) {
@@ -23,21 +24,26 @@ function game() {
 }
 
 function playRound(playerSelection, computerSelection) {
+
     let lowerPlayerSelection = playerSelection.toLowerCase(); //make case insensitivity for user
     let lowerComputerSelection = computerSelection.toLowerCase(); //make user choice with first letter capitalized. looks better when announcing winner
     let capPlayerSelection = lowerPlayerSelection.charAt(0).toUpperCase() + lowerPlayerSelection.slice(1);
+
     if ((lowerPlayerSelection === "rock" && computerSelection === "Scissors") || //win cases
         (lowerPlayerSelection === "paper" && computerSelection === "Rock")    ||
         (lowerPlayerSelection === "scissors" && computerSelection === "Paper" )) {
             playerScore += 1;
             return `You win! ${capPlayerSelection} beats ${computerSelection}. Congratulations!`;
+
         } else if ((lowerPlayerSelection === "rock" && computerSelection === "Paper") || //lose cases
                    (lowerPlayerSelection === "paper" && computerSelection === "Scissors") ||
                    (lowerPlayerSelection === "scissors" && computerSelection === "Rock")) {
                     pcScore += 1;
                         return `You lost! ${computerSelection} beats ${capPlayerSelection}. Better luck next time!`;
+
                    } else if (lowerPlayerSelection === lowerComputerSelection) { // tie case
                         return `It's a tie! Both players chose ${computerSelection}.`;
+
                    } else {
                         return `There has been an error. Your input is wrong, try again.`
                    }
